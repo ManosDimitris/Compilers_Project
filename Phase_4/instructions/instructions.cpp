334f7faf8f68a4c37a444692132503967ad35144
ManosDimitris <jimanos2004@gmail.com>
2025-06-08 12:08:30 -0400

    kai all generators

--- Phase_4/instructions/instructions.cpp
+++ Phase_4/instructions/instructions.cpp
@@ -12,6 +12,66 @@ vector<string> stringConsts;
 vector<string> namedLibfuncs;
 vector<userfunc> userFuncs;
 
+ void generate_ADD(quad*);
+void generate_SUB(quad*);
+ void generate_MUL(quad*);
+ void generate_DIV(quad*);
+ void generate_MOD(quad*);
+ void generate_UMINUS(quad*);
+ void generate_AND(quad*);
+ void generate_NEWTABLE(quad*);
+ void generate_TABLEGETELEM(quad*);
+ void generate_TABLESETELEM(quad*);
+ void generate_ASSIGN(quad*);
+ void generate_NOP(quad*);
+ void generate_JUMP(quad*);
+ void generate_IF_EQ(quad*);
+ void generate_IF_NOTEQ(quad*);
+ void generate_IF_GREATER(quad*);
+ void generate_IF_GREATEREQ(quad*);
+ void generate_IF_LESS(quad*);
+ void generate_IF_LESSEQ(quad*);
+ void generate_NOT(quad*);
+ void generate_OR(quad*);
+void generate_PARAM(quad*);
+void generate_CALL(quad*);
+void generate_GETRETVAL(quad*);
+void generate_FUNCSTART(quad*);
+void generate_RETURN(quad*);
+void generate_FUNCEND(quad*);
+
+generator_func_t generators[] = {
+    //OLA BASISMENA ME TO enum iopcode STO QUAD.HPP
+    //!! MHN ALLAXTEI KATI
+    generate_ASSIGN,
+    generate_ADD,
+    generate_MUL,
+    generate_SUB,
+    generate_DIV,
+    generate_MOD,
+    generate_UMINUS,
+    generate_AND,
+    generate_OR,
+    generate_NOT,
+    generate_IF_EQ,
+    generate_IF_NOTEQ,
+    generate_IF_LESSEQ,
+    generate_IF_GREATEREQ,
+    generate_IF_LESS,
+    generate_IF_GREATER,
+    generate_JUMP,
+    generate_CALL,
+    generate_PARAM,
+    generate_RETURN,
+    generate_GETRETVAL,
+    generate_FUNCSTART,
+    generate_FUNCEND,
+    generate_NEWTABLE,
+    generate_TABLEGETELEM,
+    generate_TABLESETELEM,
+    generate_NOP
+};
+
 unsigned int consts_newNumber(double n){
     for(unsigned int i = 0; i < numConsts.size(); i++){
         if(n == numConsts[i])
@@ -138,10 +198,60 @@ void emit(instruction instr){
     instructions.push_back(instr);
 }
 
-void generate(){
+void generate(vmopcode op, quad* quad){
+    instruction t;
+    t.opcode = op;
+
+    make_operant(quad->arg1,&t.arg1);
+    make_operant(quad->arg2,&t.arg2);
+    make_operant(quad->result,&t.result);
+    quad->taddress = nextinstructionlabel();
+    t.srcLine = quad->line;
+    emit(t);
+}
+
+
+//Extra
+void generate_Default(){
     for (int i = 0; i < quads.size(); i++)
     {   
        generators[quads[i]->op](quads[i]); 
     }
-    
 }
+
+/*Generate*/
+void generate_ADD(quad* q){generate(vmopcode(add_v), q);}
+void generate_SUB(quad* q){generate(vmopcode(add_v), q);}
+void generate_MUL(quad* q) {generate(vmopcode(mul_v), q);}
+void generate_DIV(quad* q){generate(vmopcode(div_v), q);}
+void generate_MOD(quad* q){generate(vmopcode(mod_v), q);}
+void generate_UMINUS(quad* q){generate(vmopcode(uminus_v), q);}
+void generate_AND(quad* q){generate(vmopcode(and_v), q);}
+void generate_NEWTABLE(quad* q){generate(vmopcode(newtable_v), q);}
+void generate_TABLEGETELEM(quad* q){generate(vmopcode(tablegetelem_v), q);}
+void generate_TABLESETELEM(quad* q){generate(vmopcode(tableselem_v), q);}
+void generate_ASSIGN(quad* q){generate(vmopcode(assign_v), q);}
+
+void generate_NOP(quad* q){
+    instruction t;
+    t.opcode = vmopcode(nop_v);
+    emit(t);
+}
+
+/*Generate relation*/
+void generate_JUMP(quad*){}
+void generate_IF_EQ(quad*){}
+void generate_IF_NOTEQ(quad*){}
+void generate_IF_GREATER(quad*){}
+void generate_IF_GREATEREQ(quad*){}
+void generate_IF_LESS(quad*){}
+void generate_IF_LESSEQ(quad*){}
+
+void generate_NOT(quad*){}
+void generate_OR(quad*){}
+void generate_PARAM(quad*){}
+void generate_CALL(quad*){}
+void generate_GETRETVAL(quad*){}
+void generate_FUNCSTART(quad*){}
+void generate_RETURN(quad*){}
+void generate_FUNCEND(quad*){}
--- Phase_4/instructions/instructions.hpp
+++ Phase_4/instructions/instructions.hpp
@@ -15,7 +15,7 @@ enum vmopcode{
     jle_v, jge_v, jlt_v, jgt_v, 
     call_v, pusharg_v,
     funcenter_v, funcexit_v, newtable_v,
-    tablegetelem_v, tableelem_v, nop_v
+    tablegetelem_v, tableselem_v, nop_v
 };
 
 enum vmarg_t{
@@ -51,40 +51,6 @@ struct userfunc{
     string id;                  
 };
 
-typedef void (*generator_func_t)(quad*);
-
-generator_func_t generators[] = {
-    //OLA BASISMENA ME TO enum iopcode STO QUAD.HPP
-    //!! MHN ALLAXTEI KATI
-    generate_ASSIGN,
-    generate_ADD,
-    generate_SUB,
-    generate_MUL,
-    generate_DIV,
-    generate_MOD,
-    generate_UMINUS,
-    generate_AND,
-    generate_OR,
-    generate_NOT,
-    generate_IF_EQ,
-    generate_IF_NOTEQ,
-    generate_IF_LESSEQ,
-    generate_IF_GREATEREQ,
-    generate_IF_LESS,
-    generate_IF_GREATER,
-    generate_JUMP,
-    generate_CALL,
-    generate_PARAM,
-    generate_RETURN,
-    generate_GETRETVAL,
-    generate_FUNCSTART,
-    generate_FUNCEND,
-    generate_NEWTABLE,
-    generate_TABLEGETELEM,
-    generate_TABLESETELEM,
-    generate_NOP
-};
-
 /*-------------GENERATORS-------------*/
 
 extern void generate_ADD(quad*);
@@ -115,6 +81,11 @@ extern void generate_FUNCSTART(quad*);
 extern void generate_RETURN(quad*);
 extern void generate_FUNCEND(quad*);
 
+typedef void (*generator_func_t)(quad*);
+
+extern generator_func_t generators[];
+
+
 /*------------------------------------*/
 extern vector<double> numConsts;
 extern vector<string> stringConsts;
@@ -138,6 +109,7 @@ void make_retval(vmarg* arg);
 unsigned int nextinstructionlabel();
 void emit(instruction instr);
 
-void generate();
+void generate(vmopcode op,quad* quad);
+void generate_Default();
 
 #endif
